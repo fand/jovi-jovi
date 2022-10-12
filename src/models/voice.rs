993592503d6995115ead59f619fcca3c8cbe5122
fand <mail@amagi.dev>
2022-10-12 12:01:02 +0900

    no joydivision

--- src/models/voice.rs
+++ src/models/voice.rs
@@ -7,44 +7,37 @@ pub struct Voice {
     pub is_playing: bool,
 }
 
-pub const VOICES: [Voice; 6] = [
+pub const VOICES: [Voice; 5] = [
     Voice {
         index: 0,
-        id: "JoyDivision",
-        name: "JOY DIVISION",
-        filename: "sounds/joydivision.mp3",
-        is_playing: false,
-    },
-    Voice {
-        index: 1,
         id: "Joy",
         name: "JOY",
         filename: "sounds/joy.mp3",
         is_playing: false,
     },
     Voice {
-        index: 2,
+        index: 1,
         id: "Divi",
         name: "DIVI",
         filename: "sounds/divi.mp3",
         is_playing: false,
     },
     Voice {
-        index: 3,
+        index: 2,
         id: "John",
         name: "JOHN",
         filename: "sounds/john.mp3",
         is_playing: false,
     },
     Voice {
-        index: 4,
+        index: 3,
         id: "Bon",
         name: "BON",
         filename: "sounds/bon.mp3",
         is_playing: false,
     },
     Voice {
-        index: 5,
+        index: 4,
         id: "Jovi",
         name: "JOVI",
         filename: "sounds/jovi.mp3",
